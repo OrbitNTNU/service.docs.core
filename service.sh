0ca72a2d8abbad226d455433ea93f3e3e4cf2bfd
Orbit Devops <devops.ticket@orbitntnu.com>
2024-04-03 17:25:55 +0200

    fixes

--- service.sh
+++ service.sh
@@ -12,27 +12,4 @@ source ./core/core.sh
 # BORG
 source ./borg/borg.sh
 
-# COMMANDS
-commands+=([example]="<msg>:Example command that prints <msg>")
-cmd_example() {
-  echo "Example: $1"
-}
-
-# ATTACHMENTS
-att_preStart() {
-  # execute before starting the service
-  echo "Pre Start"
-}
-
-att_configure() {
-  # execute before starting or restarting the service
-  updateTemplates
-}
-
-# FUNCTIONS
-updateTemplates() {
-  #
-  generate "example"
-}
-
 main "$@"
